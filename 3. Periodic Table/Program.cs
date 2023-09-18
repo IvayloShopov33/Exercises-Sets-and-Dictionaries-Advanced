fcf51bfecb5764af7e1287023a85f2e19e38ded0
Ivaylo Shopov <133992028+IvayloShopov33@users.noreply.github.com>
2023-09-18 23:28:54 +0300

    Update Program.cs

--- 3. Periodic Table/Program.cs
+++ 3. Periodic Table/Program.cs
@@ -11,12 +11,10 @@
                 string[] inputElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 foreach (string element in inputElements)
                 {
-                    //add the element to the sorted set if it is not present in it
                     chemicalElements.Add(element);
                 }
             }
-
-            //print the chemical elements, separated by " "
+            
             Console.WriteLine(string.Join(' ', chemicalElements));
         }
     }
